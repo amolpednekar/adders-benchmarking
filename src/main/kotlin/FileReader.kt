ce915c8494b944d6757b613b6d38c75e7ba63023
Amol Pednekar <amol0202@gmail.com>
2019-10-17 15:53:03 +0530

    FileReader utility (#2)

--- src/main/kotlin/FileReader.kt
+++ src/main/kotlin/FileReader.kt
@@ -1,4 +1,13 @@
 package main
 
-class FileReader {
+import java.io.File
+import java.io.InputStream
+import java.nio.file.Paths
+
+fun readFile(): MutableList<String>{
+    val currentPath = Paths.get("").toAbsolutePath().toString()
+    val inputStream: InputStream = File("$currentPath/src/main/resources/nodes-list").inputStream()
+    val lineList = mutableListOf<String>()
+    inputStream.bufferedReader().useLines { lines -> lines.forEach { lineList.add(it) } }
+    return lineList
 }
